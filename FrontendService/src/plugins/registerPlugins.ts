d2f0c79eae3b532d9285f52ee8f30bd05fb11713
Panic <panic.milos99@gmail.com>
2022-05-27 18:51:02 +0200

    fix: core plugins registration, support multiple at once

--- FrontendService/src/plugins/registerPlugins.ts
+++ FrontendService/src/plugins/registerPlugins.ts
@@ -1,11 +1,25 @@
 import { Plugin } from '../core/Context';
 
-export const registerPlugins = (registerPlugin: (plugin: Plugin) => any) => {
+export const registerPlugins = (registerPlugins: (plugins: Plugin[]) => any) => {
   import('.').then((module: { [key: string]: () => Plugin }) => {
+    const contextPlugins: Plugin[] = [];
+    const featurePlugins: Plugin[] = [];
+    const sidebarPlugins: Plugin[] = [];
     Object.keys(module).forEach((pluginKey: string) => {
       const getPluginDefinition = module[pluginKey];
       const plugin = getPluginDefinition();
-      registerPlugin(plugin);
+      if(plugin.type === 'SidebarPlugin')
+        sidebarPlugins.push(plugin);
+      if(plugin.type === 'ContextPlugin')
+        contextPlugins.push(plugin);
+      if(plugin.type === 'FeaturePlugin')
+        featurePlugins.push(plugin);
     });
+
+    registerPlugins([
+      ...contextPlugins,
+      ...featurePlugins,
+      ...sidebarPlugins
+    ]);
   });
 }
