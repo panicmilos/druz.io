75ea3e135899005fb06ae0a769dcdecafaf5c125
Panic <panic.milos99@gmail.com>
2022-08-11 22:34:32 +0200

    fix: fixed responses.

--- ImageService/src/route_handlers.rs
+++ ImageService/src/route_handlers.rs
@@ -1,12 +1,33 @@
 use std::path::{PathBuf, Path};
 
+use rocket::Request;
+use rocket::Response;
+use rocket::http::ContentType;
+use rocket::http::Status;
+use rocket::response;
 use rocket::response::NamedFile;
 use rocket_contrib::json::Json;
 use serde_json::{Value, json};
+use rocket::response::Responder;
 
 use super::utils;
 
 use super::requests;
+
+#[derive(Debug)]
+pub struct ApiResponse {
+    json: Json<Value>,
+    status: Status,
+}
+
+impl<'r> Responder<'r> for ApiResponse {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        Response::build_from(self.json.respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
 
 
 #[get("/<name..>", rank = 5)]
@@ -15,13 +36,13 @@ pub fn get_image(name: PathBuf) -> Option<NamedFile> {
 }
 
 #[post("/", format = "application/json", data = "<new_image>", rank = 10)]
-pub fn upload_image(new_image: Json<requests::UploadImageRequest>) -> Json<Value> {
+pub fn upload_image(new_image: Json<requests::UploadImageRequest>) -> ApiResponse {
 
     let image = new_image.into_inner().to_image();
 
     let saveImageResult = utils::save_image(&image);
     match saveImageResult {
-        Ok(_) => Json(json!({ "status": 200, "result": json!(image) })),
-        Err(errorMessage) => Json(json!({ "status": 400, "result": json!({ "message": errorMessage }) }))
+        Ok(_) => ApiResponse { json: Json(json!(image)), status: Status::Ok },
+        Err(errorMessage) => ApiResponse { json: Json(json!({ "message": errorMessage })), status: Status::BadRequest }
     }
 }
