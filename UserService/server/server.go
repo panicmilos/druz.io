07e5bcf7ee4be8f30c47c126ed1a7eaefdb1af0e
Panic <panic.milos99@gmail.com>
2022-08-19 00:31:20 +0200

    feat: additional push

--- UserService/server/server.go
+++ UserService/server/server.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/panicmilos/druz.io/UserService/controllers"
+	"github.com/panicmilos/druz.io/UserService/models"
 	"github.com/rs/cors"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -30,24 +31,24 @@ func New() *Server {
 func (server *Server) addHandlers() {
 	router := server.Router
 
-	router.Handle("/users/search", AuthenticateMiddlewate(controllers.SearchUsers)).Methods("GET")
-	router.Handle("/users/{id}", AuthenticateMiddlewate(controllers.ReadUserById)).Methods("GET")
+	router.Handle("/users/search", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.SearchUsers, models.User, models.Administrator))).Methods("GET")
+	router.Handle("/users/{id}", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.ReadUserById, models.User, models.Administrator))).Methods("GET")
 	router.Handle("/users", controllers.CreateUser).Methods("POST")
-	router.Handle("/users/{id}", controllers.UpdateUser).Methods("PUT")
-	router.Handle("/users/{id}/image", controllers.ChangeImage).Methods("PUT")
-	router.Handle("/users/{id}/password", controllers.ChangePassword).Methods("PUT")
-	router.Handle("/users/{id}/block", controllers.BlockUser).Methods("DELETE")
+	router.Handle("/users/{id}", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.UpdateUser, models.User))).Methods("PUT")
+	router.Handle("/users/{id}/image", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.ChangeImage, models.User))).Methods("PUT")
+	router.Handle("/users/{id}/password", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.ChangePassword, models.User))).Methods("PUT")
+	router.Handle("/users/{id}/block", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.BlockUser, models.Administrator))).Methods("DELETE")
 
-	router.Handle("/users/{id}/disable", controllers.DisableUser).Methods("PUT")
+	router.Handle("/users/{id}/disable", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.DisableUser, models.User))).Methods("PUT")
 	router.Handle("/users/reactivation/request", controllers.UserReactivation).Methods("POST")
 	router.Handle("/users/{id}/reactivation", controllers.ReactivateUser).Methods("PUT")
 
 	router.Handle("/users/password/recover/request", controllers.PasswordRecovery).Methods("POST")
 	router.Handle("/users/{id}/password/recover", controllers.RecoverPassword).Methods("PUT")
-	router.Handle("/users/{id}/report", AuthenticateMiddlewate(controllers.ReportUser)).Methods("POST")
+	router.Handle("/users/{id}/report", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.ReportUser, models.User))).Methods("POST")
 
-	router.Handle("/reports/search", controllers.SearchReports).Methods("GET")
-	router.Handle("/reports/{id}/ignore", controllers.IgnoreReport).Methods("DELETE")
+	router.Handle("/reports/search", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.SearchReports, models.Administrator))).Methods("GET")
+	router.Handle("/reports/{id}/ignore", AuthenticateMiddlewate(AuthorizeMiddlewate(controllers.IgnoreReport, models.Administrator))).Methods("DELETE")
 
 	router.Handle("/auth", controllers.Authenticate).Methods("POST")
 
